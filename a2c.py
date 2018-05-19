763440e6d2bfe500e98806d9cadd3c8329c35d2c
jzhanson <jzhanson@andrew.cmu.edu>
2018-05-18 21:59:58 -0700

    Saving changes before going back to single env

--- a2c.py
+++ a2c.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from skimage.transform import resize
+from PIL import Image
 import numpy as np
 from collections import deque
 
@@ -13,6 +14,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 import gym
+from gym import spaces
 
 import time
 
@@ -22,9 +24,66 @@ sess = tf.Session(config=config)
 
 # tf.set_random_seed(seed)
 
-env = gym.make('BreakoutNoFrameskip-v4')
-env = gym.wrappers.Monitor(env, './videos_a2c', force=True,
-        video_callable=lambda episode_id: episode_id%100==0)
+# Wraps Breakout environment by grayscaling and resizing the images to 84x84,
+# stacking 4 frames to form the observation, takes fire action to begin episode,
+# treats loss of life as end of an episode.
+class WrapperEnv(gym.Wrapper):
+    def __init__(self, env, num_stack=4):
+        gym.Wrapper.__init__(self, env)
+        self.num_stack = num_stack
+        shape = env.observation_space.shape
+        self.side = 84
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shape[0],
+            shape[1], self.num_stack))
+
+        self.frames = deque([], maxlen=self.num_stack)
+
+        self.lives = 0
+        self.out_of_lives = False
+        self.started = False
+
+    def grayscale_resize(self, frame):
+        #obs = resize(np.array(frame)[...,:3].dot([0.299, 0.587, 0.114]), (84, 84))
+        gray_frame = np.dot(frame.astype('float32'),
+            np.array([0.299, 0.587, 0.114], 'float32'))
+        obs = np.array(Image.fromarray(gray_frame).resize((self.side, self.side),
+                        resample=Image.BILINEAR), dtype=np.uint8)
+        return obs
+
+    def _reset(self):
+        if self.out_of_lives or not self.started:
+            frame = self.grayscale_resize(self.env.reset())
+            for i in range(self.num_stack):
+                self.frames.append(frame)
+            self.started = True
+            self.out_of_lives = False
+            obs, reward, done, info = self._step(1)
+        # Press fire at the beginning or NOOP if lives not exhausted
+        else:
+            obs, reward, done, info = self._step(0)
+
+        self.lives = self.env.unwrapped.ale.lives()
+        return self._observation()
+
+    def _step(self, action):
+        frame, reward, done, info = self.env.step(action)
+        self.out_of_lives = done
+        self.frames.append(self.grayscale_resize(frame))
+
+        lives = self.env.unwrapped.ale.lives()
+        if lives < self.lives and lives > 0:
+            done = True
+        # This if is redundant
+        if lives == 0:
+            self.out_of_lives = True
+        self.lives = lives
+
+        return self._observation(), reward, done, info
+
+    def _observation(self):
+        return np.stack(self.frames, axis=-1)
+
+
 
 # Copied from https://github.com/MG2033/A2C/blob/master/layers.py
 def openai_entropy(logits):
@@ -37,34 +96,27 @@ def openai_entropy(logits):
 
 class A2C:
     # Initialize model, variables
-    def __init__(self, lr, gamma, n):
+    def __init__(self, lr, gamma, n, num_envs=4):
         self.lr = lr
         self.n = n
         self.gamma = gamma
-        self.num_actions = env.action_space.n
+        self.num_envs = num_envs
+
+        self.envs = [gym.make('BreakoutNoFrameskip-v4') for i in
+            range(self.num_envs)]
+
+        self.envs = list(map(lambda e: WrapperEnv(e), self.envs))
+
+        self.envs = list(map(lambda e: gym.wrappers.Monitor(e, './videos_a2c', force=True,
+            video_callable=lambda episode_id: episode_id%100==0), self.envs))
+
+        self.obs_arr = []
 
         self.make_model()
 
-    def preprocess(self, frame):
-        # grayscaling
-        # 0.299, 0.587, 0.114
-        # Other option is 0.2126, 0.7152, 0.0722
-
-        gray_image = np.array(frame)[...,:3].dot([0.2126, 0.7152, 0.0722])
-
-        # magic numbers that people use to reduce images grayscale
-
-        # Resizing to 110x84 since Tensorflow doesn't care about square images
-        # Could consider downsampling in a more intelligent way
-        #
-        # Could add in anti-aliasing=true but need newest scikit-image
-        gray_small_image = resize(gray_image, (110, 84))
-
-        return gray_small_image
-
 
     def make_model(self):
-        self.input_tensor = tf.placeholder(tf.float32, shape=(None, 110, 84))
+        self.input_tensor = tf.placeholder(tf.float32, shape=(None, 84, 84, 4))
 
         self.R_tensor = tf.placeholder(tf.float32, shape=[None])
         self.A_tensor = tf.placeholder(tf.int32, shape=[None])
@@ -74,10 +126,9 @@ class A2C:
 
         self.global_step = tf.Variable(0, trainable=False)
 
-
         with tf.variable_scope("policy"):
             conv1 = tf.layers.conv2d(
-                    inputs=tf.expand_dims((tf.cast(self.input_tensor, tf.float32) / 255), -1),
+                    inputs=(tf.cast(self.input_tensor, tf.float32) / 255),
                     filters=32,
                     kernel_size=[8,8],
                     strides=4,
@@ -116,7 +167,7 @@ class A2C:
 
             self.actor_policy_logits = tf.contrib.layers.fully_connected(
                     fully_connected,
-                    env.action_space.n,
+                    self.envs[0].action_space.n,
                     activation_fn=None,
                     weights_initializer=tf.initializers.orthogonal()
             )
@@ -144,8 +195,7 @@ class A2C:
         self.actor_loss = tf.reduce_mean(self.advantage_tensor *    \
             neg_log_action_probabilities)
         self.critic_loss = tf.reduce_mean(tf.square(self.R_tensor -     \
-            tf.squeeze(self.critic_output_tensor)) / 2)
-        # Maybe this is the missing ingredient?
+            tf.squeeze(self.critic_output_tensor)))
         self.entropy = tf.reduce_mean(openai_entropy(self.actor_policy_logits))
         self.loss = self.actor_loss + 0.5 * self.critic_loss - 0.01 * self.entropy
 
@@ -173,110 +223,122 @@ class A2C:
         sess.run(tf.global_variables_initializer())
 
 
-    # Performs one episode of training in iterations of n steps each
+
+
+    # Performs one iteration of n or fewer steps on each environment
     def train(self, render=False):
-        obs = self.preprocess(env.reset())
         if render:
-            env.render()
-        done = False
-        cur_steps = 0
+            for i in range(self.num_envs):
+                self.envs[i].render()
+
+        multi_obs, multi_actions, multi_rewards, multi_dones = [[] for i \
+            in range(self.num_envs)], [[] for i in range(self.num_envs)], \
+            [[] for i in range(self.num_envs)], [[] for i in    \
+            range(self.num_envs)]
+
         total_steps = 0
-        total_reward = 0
-        total_actor_loss = 0
-        total_critic_loss = 0
-        total_entropy = 0
-        iterations = 0
 
-        while not done:
-            states = []
-            actions = []
-            rewards = []
-            dones = []
+        for i in range(self.num_envs):
+            current_obs = []
+            current_actions = []
+            current_rewards = []
+            current_dones = []
 
+            current_steps = 0
 
-            while cur_steps < self.n and not done:
-                if render:
-                    env.render()
-                states.append(obs)
+            while current_steps < self.n:
                 # Predict takes a np array of observations as the batch
                 probs = sess.run(
                     self.actor_output_tensor,
                     feed_dict={
-                        self.input_tensor: np.array([obs])  # A dirty shape hack
+                        self.input_tensor: np.array([self.obs_arr[i]])
                     },
                 )
 
+
                 #action = np.argmax(probs)
-                action = np.random.choice(env.action_space.n,
+                action = np.random.choice(self.envs[i].action_space.n,
                     p=np.ndarray.flatten(probs))
 
+                single_obs, reward, done, info = self.envs[i].step(action)
+                current_obs.append(self.obs_arr[i])
+                self.obs_arr[i] = single_obs
+                current_actions.append(action)
+                current_rewards.append(reward)
+                current_dones.append(done)
 
-                actions.append(action)
-                frame, reward, done, info = env.step(action)
-                obs = self.preprocess(frame)
-                rewards.append(reward)
-                dones.append(done)
+                current_steps += 1
 
-                cur_steps += 1
+                if done:
+                    self.obs_arr[i] = self.envs[i].reset()
+                    print('env %d done' % i)
+                    break
 
+            multi_obs[i] = current_obs
+            multi_actions[i] = current_actions
+            multi_rewards[i] = current_rewards
+            multi_dones[i] = current_dones
+
+            total_steps += current_steps
+
+
+        multi_Rs = [[] for i in range(self.num_envs)]
+        multi_values = [[] for i in range(self.num_envs)]
+
+        for i in range(self.num_envs):
             values = sess.run(
-                    self.critic_output_tensor,
-                    feed_dict={
-                        self.input_tensor: np.stack(states)
-                        },
-            )
-
+                self.critic_output_tensor,
+                feed_dict={
+                    self.input_tensor: multi_obs[i]
+                    },
+                )
             R = np.zeros_like(values)
 
             # If the episode did not end, len(rewards) == self.n, but if it did,
             # len(rewards) <= self.n
             for t in range(self.n):
-                if dones[t] == 1:
+                if multi_dones[i][t] == 1:
                     cumulative_discounted = 0
                 else:
                     cumulative_discounted = values[t]
 
-                for t_prime in range(len(rewards) - t - 1, t-1, -1):
-                    cumulative_discounted = rewards[t_prime] + self.gamma * \
-                        cumulative_discounted
+                for t_prime in range(len(multi_rewards[i]) - t - 1, t-1, -1):
+                    cumulative_discounted = multi_rewards[i][t_prime] + self.gamma * \
+                            cumulative_discounted
                 R[t] = cumulative_discounted
 
-                if dones[t] == 1:
+                if multi_dones[i][t] == 1:
                     break
 
+            multi_Rs[i] = R
+            multi_values[i] = values
 
-            '''
-            N = self.n
-            T = len(rewards)
-            exp = self.gamma ** N
-            for t in range(T-1, -1, -1):
-                v_end = 0 if t+N >= T else values[t + N]
-                cumulative = 0
-                for k in range(N-1):
-                    cumulative += (self.gamma ** k) * (rewards[t+k] if t+k < T else 0)
-                R[t] = exp * v_end + cumulative
-            '''
+        multi_obs = np.array(multi_obs)
+        multi_actions = np.array(multi_actions)
+        multi_Rs = np.array(multi_Rs)
+        multi_values = np.array(multi_values)
 
-            actor_loss, critic_loss, entropy, _ = sess.run(
-                    [self.actor_loss, self.critic_loss, self.entropy, self.train_both],
-                    feed_dict={
-                        self.input_tensor: np.stack(states),
-                        self.A_tensor: actions,
-                        self.R_tensor: R.reshape((-1)),
-                        self.advantage_tensor: np.ndarray.flatten(R - values),
-                    },
-            )
+        print(multi_obs.shape)
+        print(multi_actions.shape)
 
-            total_steps += cur_steps
-            total_reward += np.sum(rewards)
-            total_actor_loss += actor_loss
-            total_critic_loss += critic_loss
-            total_entropy += entropy
-            iterations += 1
-            cur_steps = 0
+        print(multi_Rs.shape)
+        print(multi_values.shape)
 
-        return (total_actor_loss / iterations, total_critic_loss / iterations,
-            total_entropy / iterations, total_reward, total_steps, iterations)
+        actor_loss, critic_loss, entropy, _ = sess.run(
+                [self.actor_loss, self.critic_loss, self.entropy, self.train_both],
+                feed_dict={
+                    self.input_tensor: multi_obs.reshape(-1, *multi_obs.shape[-3:]),
+                    self.A_tensor: np.ndarray.flatten(multi_actions),
+                    self.R_tensor: np.ndarray.flatten(multi_Rs),
+                    self.advantage_tensor: np.ndarray.flatten(multi_Rs - multi_values),
+                },
+        )
+
+        return actor_loss, critic_loss, entropy, np.sum(multi_rewards) /    \
+            self.num_envs, np.sum(list(map(lambda l : len(l), multi_rewards))),\
+            4*(total_steps // self.n + 1)
+
+
 
 
     def test(self):
@@ -314,21 +376,24 @@ def main(args):
     current_total_critic_loss = 0
     current_total_entropy = 0
 
+
+    a2c.obs_arr = np.array(list(map(lambda env: env.reset(), a2c.envs)))
+
     for episode in range(num_episodes):
-        loss, critic_loss, entropy, total_reward, episode_len, iterations = a2c.train()
+        loss, critic_loss, entropy, avg_reward, avg_episode_len, iterations = a2c.train()
         total_iterations += iterations
         print('#' * 50)
         print('Episode: %d' % episode)
-        print('Reward: %f' % total_reward)
-        print('Steps: %d' % episode_len)
+        print('Reward: %f' % avg_reward)
+        print('Steps: %f' % avg_episode_len)
         print('Loss: %f' % loss)
         print('Critic loss: %f' % critic_loss)
         print('Entropy: %f' % entropy)
         print('Iterations: %d' % iterations)
         print('Total iterations: %d' % total_iterations)
 
-        current_total_reward += total_reward
-        current_total_ep_len += episode_len
+        current_total_reward += avg_reward
+        current_total_ep_len += avg_episode_len
         current_total_loss += loss
         current_total_critic_loss += critic_loss
         current_total_entropy += entropy
@@ -383,7 +448,6 @@ def main(args):
             plt.xlabel('Episodes')
             plt.ylabel('Average entropy per 100 episodes')
             plt.savefig('entropy_a2c.png')
-
 
 
 
